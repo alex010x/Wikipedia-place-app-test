811ba332180118ab1df4b27de21537a82fb7dccf
Alessandro Minopoli <alessandro.minopoli@globant.com>
2026-02-26 22:00:16 +0100

    Initial Commit

---
+++ Wikipedia-place-app-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wikipedia-place-app-test
+//
+//  Created by Alessandro Minopoli on 26/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wikipedia-place-app-test/Wikipedia_place_app_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wikipedia_place_app_testApp.swift
+//  Wikipedia-place-app-test
+//
+//  Created by Alessandro Minopoli on 26/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Wikipedia_place_app_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
